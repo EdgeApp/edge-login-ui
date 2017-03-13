38d61793ac61517f08ad6014b9fba75efa3694c8
Kylan Hurt <kylanhurt@Kylans-MBP.airbitz.co>
2017-03-13 13:07:18 -0700

    Modify 'Invalid OTP Token' error modal text

--- src/modules/ErrorModal/ErrorModal.action.js
+++ src/modules/ErrorModal/ErrorModal.action.js
@@ -1,10 +1,10 @@
 export const ERROR_MODAL_OPEN = 'ERROR_MODAL_OPEN'
 export const ERROR_MODAL_CLOSE = 'ERROR_MODAL_CLOSE'
 
-export function openErrorModal (message) {
+export function openErrorModal (error) {
   return {
     type: ERROR_MODAL_OPEN,
-    message
+    error
   }
 }
 
--- src/modules/ErrorModal/ErrorModal.reducer.js
+++ src/modules/ErrorModal/ErrorModal.reducer.js
@@ -13,10 +13,20 @@ export const visible = (state = false, action) => {
   }
 }
 
-export const message = (state = '', action) => {
+export const message = (state = '', action) => {  
   switch (action.type) {
     case ACTION.ERROR_MODAL_OPEN :
-      return action.message
+      switch (action.error.type) {
+        case 'OtpError':
+          return "This account has Two Factor Authentication enabled and cannot be logged in using a username and password. Please login by scanning the barcode with the Airbitz Wallet."
+        case 'PasswordError':
+          return "Invalid password."
+        default:
+          return action.message
+          return action.error.message
+          return action.error
+      }
+
 
     case ACTION.ERROR_MODAL_CLOSE :
       return ''
