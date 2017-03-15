5b9a51f77239c19ef5f8de608f852eb78a66a3fd
Art Manalili <artmanalili@gmail.com>
2017-03-15 17:10:53 +0800

    Fix error modal reducer. Having errors because there is business logic

--- src/modules/ErrorModal/ErrorModal.reducer.js
+++ src/modules/ErrorModal/ErrorModal.reducer.js
@@ -13,20 +13,10 @@ export const visible = (state = false, action) => {
   }
 }
 
-export const message = (state = '', action) => {  
+export const message = (state = '', action) => {
   switch (action.type) {
     case ACTION.ERROR_MODAL_OPEN :
-      switch (action.error.type) {
-        case 'OtpError':
-          return "This account has Two Factor Authentication enabled and cannot be logged in using a username and password. Please login by scanning the barcode with the Airbitz Wallet."
-        case 'PasswordError':
-          return "Invalid password."
-        default:
-          return action.message
-          return action.error.message
-          return action.error
-      }
-
+      return action.message
 
     case ACTION.ERROR_MODAL_CLOSE :
       return ''
