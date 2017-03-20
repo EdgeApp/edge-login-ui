5a27f5544b8881dcaab096196d8e43ac6824f3a1
Art Manalili <artmanalili@gmail.com>
2017-03-20 13:43:29 +0800

    Put context.checkPasswordRule() to the validations and the password section of sign up modal

--- src/modules/Password/Password.middleware.js
+++ src/modules/Password/Password.middleware.js
@@ -5,19 +5,24 @@ import { signupUser } from '../Signup/Signup.middleware'
 export const checkPassword = (password, passwordRepeat, validation, username, pinNumber, callback) => {
   return (dispatch, getState, imports) => {
     const t = imports.t
-    if (!validation.upperCaseChar || !validation.lowerCaseChar || !validation.number || !validation.characterLength) {
-      return dispatch(openErrorModal(t('activity_signup_insufficient_password')))
-    }
-    if (password !== passwordRepeat) {
-      return dispatch(openErrorModal(t('activity_signup_passwords_dont_match')))
-    }
-    if (validation.upperCaseChar && validation.lowerCaseChar && validation.number && validation.characterLength && password === passwordRepeat) {
-      return dispatch(
-        signupUser(username, password, pinNumber, callback)
-      )
-    } else {
-      return dispatch(openErrorModal(t('activity_signup_insufficient_password')))
-    }
+
+    imports.abcContext(context => {
+      const check = context.checkPasswordRules(password)
+      if (!check.passed) {
+        return dispatch(openErrorModal(t('activity_signup_insufficient_password')))
+      }
+      if (password !== passwordRepeat) {
+        return dispatch(openErrorModal(t('activity_signup_passwords_dont_match')))
+      }
+      if (check.passed && password === passwordRepeat) {
+        return dispatch(
+          signupUser(username, password, pinNumber, callback)
+        )
+      } else {
+        return dispatch(openErrorModal(t('activity_signup_insufficient_password')))
+      }
+    })
+
   }
 }
 
