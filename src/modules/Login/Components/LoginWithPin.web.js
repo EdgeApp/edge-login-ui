863117765653a3b886c877746a0f59dd28c846fc
Art <art@Arts-MacBook-Pro.local>
2017-07-28 00:26:34 +0800

    Added the inputs the login with pin screen. Inputs not yet fully detailed and customize

--- src/modules/Login/Components/LoginWithPin.web.js
+++ src/modules/Login/Components/LoginWithPin.web.js
@@ -9,7 +9,23 @@ class LoginWithPin extends Component {
         <div className={styles.background}></div>
         <div className={styles.main}>
           <p className={styles.header}>Login with your PIN</p>
-          <div className={styles.box}></div>
+          <div className={styles.box}>
+            <div className={styles.userList}>
+              <select>
+                <option>Areallylongexampleusernamethatfits</option>
+                <option>SuperDuperAwesomeText</option>
+                <option>GokuVegetaKririnYamcha</option>
+                <option>BayangMagiliwPerlasNgSilanganan</option>
+              </select>
+              <label className={styles['control-label']} htmlFor="select">Username</label>
+              <i className={styles.bar}></i>
+            </div>
+            <div className={styles.password}>
+              <input type="password" style={{ textAlign: 'center' }}required="required"/>
+              <label className={styles.passwordLabel} htmlFor="input">Enter PIN</label>
+              <i className={styles.bar}></i>
+            </div>
+          </div>
           <p className={styles.exitLink}>Exit PIN Login</p>
         </div>
       </div>
