bb1eabff3f42a43ff61c1958347486c8dea30a9b
Art <art@Arts-MacBook-Pro.local>
2017-07-31 14:17:59 +0800

    Finished the base modal, added the password recovery modal

--- src/modules/Modals/PasswordRecovery/PasswordRecovery.web.js
+++ src/modules/Modals/PasswordRecovery/PasswordRecovery.web.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import Modal from '../BaseModal.web.js'
-// import styles from './PasswordRecovery.webStyle.scss'
+import styles from './PasswordRecovery.webStyle.scss'
 
 class PasswordRecovery extends Component {
   render () {
@@ -12,7 +12,16 @@ class PasswordRecovery extends Component {
         close={this.props.close}
         icon='../../../../assets/modals/recovery-W.png'
       >
-        <p>test</p>
+        <p className={styles.header}>Password Recovery</p>
+        <p className={styles.text}>
+          If recovery was setup, you should have emailed yourself a recovery token with a link.
+          To recover your account, install the Airbitz Mobile App on iOS or Android at: <br />
+          <a target="_blank" href="https://airbitz.co/app" className={styles.linked}>https://airbitz.co/app</a>
+        </p>
+        <p className={styles.text}>
+         Then click one of the links in the recovery email from a device with Airbitz installed.
+        </p>
+        <button className={styles.close} onClick={this.props.close}>Ok</button>
       </Modal>
     )
   }
