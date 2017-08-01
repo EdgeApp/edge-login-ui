d7165fd6c3122512aab01e300c15e34fb68aec91
William Swanson <swansontec@gmail.com>
2017-08-01 04:44:03 -0700

    Upgrade webpack and related plugins to version 3
    This allows us to clean things up a ton.

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+var path = require('path')
+var webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const production = process.env.NODE_ENV === 'production'
+
+// Only include these plugins in production builds:
+const productionPlugins = production
+  ? [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true,
+      compress: {
+        drop_console: true
+      },
+      mangle: {
+        except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad']
+      }
+    })
+  ]
+  : []
+
+// Only include these plugins in production builds:
+module.exports = {
+  devtool: production ? 'cheap-module-source-map' : 'inline-source-map',
+  devServer: {
+    contentBase: './assets',
+    port: 8002
+  },
+
+  entry: './src/index.web.js',
+
+  output: {
+    path: path.join(__dirname, 'assets'),
+    filename: 'app.[name].js',
+    publicPath: '/'
+  },
+
+  resolve: {
+    extensions: ['.scss', '.css', '.js', '.jsx', '.json'],
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, './node_modules'),
+      path.resolve('./src')
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin('bundle.css', { allChunks: true }),
+    ...productionPlugins,
+    new HtmlWebpackPlugin({
+      filename: 'sample-iframe.html',
+      template: 'src/sample/sample-iframe.html'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'src'),
+        use: production
+          ? ['babel-loader']
+          : ['react-hot-loader', 'babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        include: [path.join(__dirname, 'src'), /node_modules\/react-toolbox\//],
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                modules: true,
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [autoprefixer]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                data: '@import "theme/_config.scss";',
+                includePaths: [path.resolve(__dirname, './src')]
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(gif|jpe?g|png|svg)$/i,
+        include: path.join(__dirname, 'src'),
+        loader: 'file-loader',
+        options: {
+          name: '/assets/[name].[hash].[ext]'
+        }
+      }
+    ]
+  }
+}
