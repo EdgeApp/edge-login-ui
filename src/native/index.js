7f53a611a466868534259b9548f2c012c8bdabbd
William Swanson <swansontec@gmail.com>
2017-08-01 15:48:53 -0700

    Export a separate React Native library (empty now)

---
+++ src/native/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+export class LoginScreen extends Component {
+  render () {
+    return <Text>Imagine a login window here...</Text>
+  }
+}
