7c8b586c9901fa64edcc172cdbce06591ac960b4
Art <art@Arts-MacBook-Pro.local>
2017-08-10 23:28:35 +0800

    Added more states in the signup password, the password requirements mostly.

--- src/modules/Password/Password.web.new.js
+++ src/modules/Password/Password.web.new.js
@@ -6,30 +6,126 @@ import FontIcon from 'react-toolbox/lib/font_icon'
 import Input from 'react-toolbox/lib/input'
 import styles from './Password.webStyle.scss'
 
+import { validate } from './PasswordValidation/PasswordValidation.middleware'
+import { checkPassword, skipPassword } from './Password.middleware'
+import { changeSignupPage } from '../Signup/Signup.action'
+import {
+  passwordNotificationShow,
+  showPassword,
+  hidePassword,
+  changePasswordValue,
+  changePasswordRepeatValue
+} from './Password.action'
+
 import eyeShow from '../../img/create-account/show-password.png'
-// import eyeHide from '../../img/create-account/hide-password.png'
+import eyeHide from '../../img/create-account/hide-password.png'
 
 class Password extends Component {
+  _handleSubmit = () => {
+    const callback = () => this.props.router.push('/review')
+    this.props.dispatch(
+      checkPassword(
+        this.props.password,
+        this.props.passwordRepeat,
+        this.props.validation,
+        this.props.username,
+        this.props.pin,
+        callback
+      )
+    )
+  }
+  _handleSubmitSkipPassword = () => {
+    const callback = () => this.props.router.push('/review')
+    this.props.dispatch(
+      skipPassword(
+        this.props.username,
+        this.props.pin,
+        callback
+      )
+    )
+  }
+  _handleBack = () => {
+    if (this.props.loader.loading === false) {
+      return this.props.dispatch(changeSignupPage('pin'))
+    }
+  }
+  _handlePasswordNotification = () => {
+    this.props.dispatch(passwordNotificationShow())
+  }
+  passwordKeyPressed = (e) => {
+    if (e.charCode === 13) {
+      this.refs.passwordRepeat.getWrappedInstance().focus()
+    }
+  }
+  _handleOnChangePassword = (password) => {
+    this.props.dispatch(changePasswordValue(password))
+    this.props.dispatch(validate(password))
+  }
+  _handleOnChangePasswordRepeat = (passwordRepeat) => {
+    this.props.dispatch(changePasswordRepeatValue(passwordRepeat))
+  }
+  toggleRevealPassword = (e) => {
+    if (this.props.inputState) {
+      return this.props.dispatch(hidePassword())
+    } else {
+      return this.props.dispatch(showPassword())
+    }
+  }
+  _handleKeyEnter = (e) => {
+    if (e.nativeEvent.charCode === 13) {
+      return this._handleSubmit()
+    }
+  }
   render () {
+    const { inputState, validation, password, passwordRepeat } = this.props
     return (
       <div className={styles.container}>
         <p className={styles.header}>Change your password</p>
-        <p>The password is used to authenticate your account and to change sensitive settings.</p>
+        <p className={styles.hint}>The password is used to authenticate your account and to change sensitive settings.</p>
         <div className={styles.main}>
           <div className={styles.tooltip}>
             <p className={styles.textHeader}>Password Requirements:</p>
-            <p className={styles.text}><FontIcon value='clear' className={styles.icon} />Must have at least one upper case letter</p>
-            <p className={styles.text}><FontIcon value='clear' className={styles.icon} />Must have at least one lower case letter</p>
-            <p className={styles.text}><FontIcon value='clear' className={styles.icon} />Must have at least one number</p>
-            <p className={styles.text}><FontIcon value='clear' className={styles.icon} />Must have at least 10 characters</p>
+            <p className={styles.text}>
+              {validation.upperCaseChar ? <FontIcon value='done' className={styles.check} /> : <span className={styles.bullet}>•</span>}
+              Must have at least one upper case letter
+            </p>
+            <p className={styles.text}>
+              {validation.lowerCaseChar ? <FontIcon value='done' className={styles.check} /> : <span className={styles.bullet}>•</span>}
+              Must have at least one lower case letter
+            </p>
+            <p className={styles.text}>
+              {validation.number ? <FontIcon value='done' className={styles.check} /> : <span className={styles.bullet}>•</span>}
+              Must have at least one number
+            </p>
+            <p className={styles.text}>
+              {validation.characterLength ? <FontIcon value='done' className={styles.check} /> : <span className={styles.bullet}>•</span>}
+              Must have at least 10 characters
+            </p>
             <p className={styles.crack}>Time to crack: 0 seconds</p>
           </div>
           <div className={styles.formContainer}>
             <div className={styles.formWithIcon}>
-              <Input type='password' label='Password' name='password' className={styles.form} />
-              <img src={eyeShow} className={styles.icon} />
+              <Input
+                autoFocus
+                name='password'
+                label='Password'
+                type={inputState ? 'text' : 'password'}
+                onKeyPress={this.passwordKeyPressed}
+                onChange={this._handleOnChangePassword}
+                value={password}
+                className={styles.form}
+              />
+              <img src={inputState ? eyeHide : eyeShow} className={styles.icon} onClick={this.toggleRevealPassword} />
             </div>
-            <Input type='password' label='Re-enter Password' name='password_confirmation' className={styles.form} />
+            <Input
+              name='passwordRepeat'
+              label='Re-enter Password'
+              type={inputState ? 'text' : 'password'}
+              onKeyPress={this._handleKeyEnter.bind(this)}
+              onChange={this._handleOnChangePasswordRepeat}
+              value={passwordRepeat}
+              className={styles.form}
+            />
           </div>
         </div>
         <div className={styles.rowButtons}>
@@ -43,4 +139,12 @@ class Password extends Component {
   }
 }
 
-export default connect()(Password)
+export default connect(state => ({
+  inputState: state.password.inputState,
+  password: state.password.password,
+  passwordRepeat: state.password.passwordRepeat,
+  validation: state.password.validation,
+  username: state.username,
+  pin: state.pin,
+  loader: state.loader
+}))(Password)
