821223dc022bdfe3427d8879b151d8cace330889
Art <artmanalili@gmail.com>
2017-09-03 19:39:41 +0800

    Edited abcui to accomodate internal and external ui, still on testing and changing phase

--- src/modules/Header/Header.web.js
+++ src/modules/Header/Header.web.js
@@ -5,15 +5,8 @@ import { sprintf } from 'sprintf-js'
 import t from 'lib/web/LocaleStrings'
 
 class Header extends Component {
-  componentWillMount () {
-    try {
-      const vendorName = window.parent.abcuiContext.vendorName || 'Sample App'
-      this.props.vendorName = vendorName
-    } catch (e) {
-      console.log('Error with finding vendorName: ', e)
-    }
-  }
   _renderheader = (pathname) => {
+    const name = window.parent.abcui.vendorName || window.abcui.vendorName
     switch (pathname) {
       case '/changepin':
         return 'Change your 4-digit PIN'
@@ -22,7 +15,7 @@ class Header extends Component {
       case '/passwordrecovery':
         return 'Password Recovery Setup'
       default:
-        return sprintf(t('fragment_setup_header_left'), this.props.vendorName)
+        return sprintf(t('fragment_setup_header_left'), name)
     }
   }
   render () {
