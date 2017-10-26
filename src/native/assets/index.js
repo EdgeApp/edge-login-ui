ed28fdf7f6c9d8dc83983c79e729ba16a2b757e5
“Allen <allen@missionvi.com>
2017-10-26 10:00:06 -0700

    swap out of icons for checks

--- src/native/assets/index.js
+++ src/native/assets/index.js
@@ -2,12 +2,14 @@ const LOGIN_BACKGROUND = require('./login_bg.png')
 const LOGO_DOT = require('./logoHolderDot.png')
 const LOGO_BIG = require('./placeholder_logo_L.png')
 const LOGO_SMALL = require('./placeholder_logo_S.png')
-const REVIEW_CHECKED = require('./icons/review/checked.png')
+const REVIEW_CHECKED = require('./icons/review/check.png')
 const REVIEW_UNCHECKED = require('./icons/review/unchecked.png')
-const STANDARD_CHECKED = require('./icons/standard/checked.png')
-const STANDARD_UNCHECKED = require('./icons/standard/unchecked.png')
+const STANDARD_CHECKED = require('./icons/standard/password_show.png')
+const STANDARD_UNCHECKED = require('./icons/standard/password_hide.png')
 const PASSWORD_REQ_CHECKED = require('./icons/passwordRequirements/checked.png')
 const PASSWORD_REQ_UNCHECKED = require('./icons/passwordRequirements/unchecked.png')
+const PERMISSIONS_CAMREA = require('./icons/permissions/permissions-camera.png')
+const WELCOME = require('./icons/welcome/new_account_welcome.png')
 
 export {LOGIN_BACKGROUND}
 export {LOGO_BIG}
@@ -19,4 +21,6 @@ export {STANDARD_CHECKED}
 export {STANDARD_UNCHECKED}
 export {PASSWORD_REQ_CHECKED}
 export {PASSWORD_REQ_UNCHECKED}
+export {PERMISSIONS_CAMREA}
+export {WELCOME}
 
