ee2ae98ded0e38d0575e6ff311f5951f9ab2b43a
Allen Hujsak <allenhujsak@Allens-MacBook-Air.local>
2017-11-14 00:12:58 -0800

    adjust width for input fields

--- src/common/constants/Theme.js
+++ src/common/constants/Theme.js
@@ -2,13 +2,13 @@
 export const HEADER_HEIGHT = 66
 
 export const BUTTON_HEIGHT = 44
-export const BUTTON_WIDTH_1 = 220
+export const BUTTON_WIDTH_1 = '70%'
 export const BUTTON_100_PERCENT = '100%'
 export const BUTTON_BORDER_STROKE = 1
 export const BUTTON_BORDER_RADIUS = 3
 
 // Login Screen
-export const LOGIN_LABEL_WIDTH = 230
+export const LOGIN_LABEL_WIDTH = '70%'
 export const LOGIN_LABEL_HEIGHT = 60
 export const LOGIN_TEXT_ONLY_BUTTON_TEXT_HEIGHT = 14
 
