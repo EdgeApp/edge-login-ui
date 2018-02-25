4c8acec1a757818a57fbeaf724090f181ffbcb45
Art Manalili <artmanalili@gmail.com>
2018-02-26 05:05:59 +0800

    Change loading screen to landing page background for smooth transition from landing page to login and logging out

--- packages/edge-login-ui-rn/src/native/components/screens/LoadingScreenComponent.js
+++ packages/edge-login-ui-rn/src/native/components/screens/LoadingScreenComponent.js
@@ -1,13 +1,28 @@
 // @flow
 import React, { Component } from 'react'
-import { Spinner } from '../common'
+import { View } from 'react-native'
+import { BackgroundImage, Spinner } from '../common'
+import * as Assets from '../../assets/'
 
 type Props = {
   styles: Object
 }
 
-export default class LandingScreenComponent extends Component<Props> {
+export default class LoadingScreenComponent extends Component<Props> {
   render () {
+    const { LandingScreenStyle } = this.props.styles
+    return (
+      <View style={LandingScreenStyle.container}>
+        <BackgroundImage
+          src={Assets.LOGIN_BACKGROUND}
+          style={LandingScreenStyle.backgroundImage}
+          content={this.renderSpinner()}
+        />
+      </View>
+    )
+  }
+
+  renderSpinner = () => {
     const { CenteredSpinnerStyle } = this.props.styles
     return <Spinner style={CenteredSpinnerStyle} />
   }
