9415c5cb4f1e4532802b9c2cd31d2352c3da981c
Allen Hujsak <allenhujsak@Allens-MacBook-Air.local>
2018-03-19 10:11:33 -0400

    migrate localization methords from GUI project to react native project.

---
+++ packages/edge-login-ui-rn/src/common/locales/en_US.js
@@ -0,0 +1,13 @@
+// @flow
+
+const strings = {
+  login_button: 'Login',
+  username: 'Username',
+  password: 'Password',
+  forgot_password: 'Forgot Password',
+  create_an_account: 'Create an account',
+  initiate_password_recovery: 'Please find the email and click on the link from this device to initiate recovery. ',
+  if_recovery_modal: 'If recovery was set up, you should have emailed yourself a recovery token with a link.'
+}
+
+export default strings
