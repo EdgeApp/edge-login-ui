9068a4dacff875d41412bb07e8f3220fff7b4c84
William Swanson <swansontec@gmail.com>
2018-04-02 15:16:06 -0700

    web: Fix the copy-edge-assets script

--- packages/edge-login-ui-web/src/copy-edge-assets.js
+++ packages/edge-login-ui-web/src/copy-edge-assets.js
@@ -7,7 +7,7 @@ function main (argv) {
   if (argv.length !== 3) {
     return console.error('Usage: copy-edge-assets <dest>')
   }
-  const src = path.join(__dirname, 'assets')
+  const src = path.join(__dirname, '../assets')
   const dest = path.resolve(argv[2])
 
   console.log(src + ' -> ' + dest)
