b962fe96f3f32ac2e352fbee79d3f30d02d81eb6
William Swanson <swansontec@gmail.com>
2018-04-05 15:08:57 -0700

    web: Move frame selectors to their own file

---
+++ packages/edge-login-ui-web/src/frame/frame-selectors.js
@@ -0,0 +1,39 @@
+// @flow
+
+import type { EdgeUserInfos } from '../edge-types.js'
+import type { FrameState } from './frame-state.js'
+
+/**
+ * Builds a table of users that are available on this device.
+ */
+export async function getLocalUsers (state: FrameState): EdgeUserInfos {
+  const usernames: Array<string> = await state.context.listUsernames()
+
+  const out: EdgeUserInfos = {}
+  for (const username of usernames) {
+    out[username] = {
+      hasPin: await state.context.pinLoginEnabled(username),
+      username
+    }
+  }
+  return out
+}
+
+/**
+ * Grabs the wallet infos out of an account object, sanitizing them as needed.
+ */
+export function getWalletInfos (state: FrameState, accountId: string) {
+  const account = state.accounts[accountId]
+  const locked: boolean = false
+
+  const out = {}
+  for (const walletInfo of account.allKeys) {
+    const { type, id, archived, deleted, sortIndex } = walletInfo
+    out[walletInfo.id] = { type, id, archived, deleted, sortIndex }
+    if (!locked) {
+      out[walletInfo.id].keys = walletInfo.keys
+      out[walletInfo.id].appIds = walletInfo.appIds
+    }
+  }
+  return out
+}
