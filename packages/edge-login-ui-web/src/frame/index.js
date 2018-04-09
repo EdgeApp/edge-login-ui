15924b22f24cc1c04c125e7817cae563b4080c93
William Swanson <swansontec@gmail.com>
2018-04-09 14:09:37 -0700

    web: Fix the iframe HMR crashes

--- packages/edge-login-ui-web/src/frame/index.js
+++ packages/edge-login-ui-web/src/frame/index.js
@@ -4,4 +4,8 @@ import 'regenerator-runtime/runtime'
 
 import { awaitConnection } from './frame-state.js'
 
-awaitConnection()
+const listener = awaitConnection()
+
+// Close the post-robot listener if our page reloads during development:
+// $FlowFixMe
+if (module.hot) module.hot.dispose(() => listener.cancel())
