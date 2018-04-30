63478eb136e163fd4e53fea5d42ecb2533d5e6ec
Art Manalili <artmanalili@gmail.com>
2018-04-30 18:53:28 +0800

    react: Change some Airbitz string to Edge, Added username error handling

--- packages/edge-login-ui-react/src/modules/Signup/Username/Username.middleware.js
+++ packages/edge-login-ui-react/src/modules/Signup/Username/Username.middleware.js
@@ -3,17 +3,22 @@ import { closeLoading, openLoading } from '../../Loader/Loader.action'
 export const checkUsername = (username, callback) => {
   return (dispatch, getState, imports) => {
     const t = imports.t
+    const { usernameAvailable } = window.abcui.abcuiContext
     dispatch(openLoading(t('activity_signup_checking_username')))
 
-    const context = window.abcui.abcuiContext
-    context.usernameAvailable(username).then(available => {
-      dispatch(closeLoading())
-      if (!available) {
-        return callback(t('activity_signup_username_unavailable'))
-      }
-      if (available) {
-        return callback(null)
-      }
-    })
+    usernameAvailable(username)
+      .then(available => {
+        dispatch(closeLoading())
+        if (!available) {
+          return callback(t('activity_signup_username_unavailable'))
+        }
+        if (available) {
+          return callback(null)
+        }
+      })
+      .catch(error => {
+        dispatch(closeLoading())
+        return callback(error.message)
+      })
   }
 }
