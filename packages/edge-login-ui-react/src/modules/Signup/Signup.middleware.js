582880fd1996e29984b80cdb5c8b2855fe62504e
William Swanson <swansontec@gmail.com>
2018-08-17 15:20:48 -0700

    Stop using deprecated/removed core features

--- packages/edge-login-ui-react/src/modules/Signup/Signup.middleware.js
+++ packages/edge-login-ui-react/src/modules/Signup/Signup.middleware.js
@@ -1,5 +1,5 @@
 import { closeLoading, openLoading } from '../Loader/Loader.action'
-import { loginWithPassword } from '../Login/Login.middleware.js'
+import { userLogin } from '../Login/Login.action.js'
 
 export const signupUser = (username, password, pin, callback) => {
   return (dispatch, getState, imports) => {
@@ -7,11 +7,16 @@ export const signupUser = (username, password, pin, callback) => {
     dispatch(openLoading(t('fragment_signup_creating_account')))
 
     const ctx = window.abcui.abcuiContext
-    ctx.createAccount(username, password, pin, undefined, (err, result) => {
-      dispatch(closeLoading())
-      if (!err) {
-        return dispatch(loginWithPassword(username, password, callback))
-      }
-    })
+    ctx
+      .createAccount(username, password, pin)
+      .then(account => {
+        localStorage.setItem('lastUser', username)
+        dispatch(userLogin(account))
+        dispatch(closeLoading())
+        callback(null, account)
+      })
+      .catch(e => {
+        dispatch(closeLoading())
+      })
   }
 }
