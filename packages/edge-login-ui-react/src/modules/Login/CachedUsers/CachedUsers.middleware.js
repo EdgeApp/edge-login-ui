bc948623199c84191638e294e5919a13c190b74a
William Swanson <swansontec@gmail.com>
2018-09-06 12:12:14 -0700

    react: Fix removing usernames from the device

--- packages/edge-login-ui-react/src/modules/Login/CachedUsers/CachedUsers.middleware.js
+++ packages/edge-login-ui-react/src/modules/Login/CachedUsers/CachedUsers.middleware.js
@@ -5,8 +5,14 @@ export const deleteUserToCache = username => {
     const localStorage = global ? global.localStorage : window.localStorage
     const lastUser = localStorage.getItem('lastUser')
 
-    window.abcui.abcuiContext.removeUsername(username)
-    if (lastUser === username) localStorage.removeItem('lastUser')
-    dispatch(deleteUserFromUserCache(username))
+    window.abcui.abcuiContext
+      .deleteLocalAccount(username)
+      .then(() => {
+        if (lastUser === username) localStorage.removeItem('lastUser')
+        dispatch(deleteUserFromUserCache(username))
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
 }
