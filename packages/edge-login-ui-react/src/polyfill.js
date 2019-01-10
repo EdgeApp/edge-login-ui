ca4d1f52e2c97ccdaf33e47de2e1270e0dc38b63
Shaun <tbevan@gmail.com>
2019-01-10 10:37:55 -0500

    Login only

--- packages/edge-login-ui-react/src/polyfill.js
+++
@@ -1,8 +0,0 @@
-import 'whatwg-fetch'
-
-String.format = function (format) {
-  const args = Array.prototype.slice.call(arguments, 1)
-  return format.replace(/{(\d+)}/g, function (match, number) {
-    return typeof args[number] !== 'undefined' ? args[number] : match
-  })
-}
--- packages/edge-login-ui-react/src/services/date.js
+++
@@ -1,5 +0,0 @@
-import moment from 'moment'
-
-export default date => {
-  return date ? moment(date).format('MMMM DD, YYYY hh:mm:ss A') : ''
-}
