5354986ad0cc14053422648107d50fd1b0ea139c
William Swanson <swansontec@gmail.com>
2020-08-12 12:05:07 -0700

    rn: Upgrade to edge-core-js v0.17.9

--- packages/edge-login-ui-rn/src/types/ReduxActions.js
+++ packages/edge-login-ui-rn/src/types/ReduxActions.js
@@ -93,7 +93,13 @@ export type Action =
       }
     }
   | { type: 'ON_RECOVERY_LOGIN_NOT_ENABLED', data?: string /* error */ }
-  | { type: 'OTP_ERROR', data: OtpError }
+  | {
+      type: 'OTP_ERROR',
+      data: OtpError & {
+        loginAttempt: 'PASSWORD' | 'PIN' | 'RECOVERY',
+        loginAttemptData?: string[]
+      }
+    }
   | { type: 'OTP_LOGIN_BACKUPKEY_FAIL', data: string }
   | { type: 'OTP_RESET_REQUEST', data: Date }
   | {
