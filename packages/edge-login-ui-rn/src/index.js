bff7a5a92e003f0c7e2205f359fcae5e1b811ce1
William Swanson <swansontec@gmail.com>
2021-01-19 12:49:35 -0800

    rn: Remove the `ChooseTestAppScreen`

--- packages/edge-login-ui-rn/src/index.js
+++ packages/edge-login-ui-rn/src/index.js
@@ -2,7 +2,6 @@
 
 import './util/androidFetch.js'
 
-export * from './components/publicApi/ChooseTestAppScreen.js'
 export * from './components/publicApi/PublicChangePasswordScreen.js'
 export * from './components/publicApi/PublicChangePinScreen.js'
 export * from './components/publicApi/PublicChangeRecoveryScreen.js'
