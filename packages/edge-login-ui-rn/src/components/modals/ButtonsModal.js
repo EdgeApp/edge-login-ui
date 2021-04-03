8873be3cc26b3aeba2cbb54af06bee094f65de38
William Swanson <swansontec@gmail.com>
2021-04-02 17:43:10 -0700

    Add several features to the `ButtonsModal`
    - Arbitrary children for enhanced messages.
    - Spinners on buttons.
    - Optional modal close arrow.

--- packages/edge-login-ui-rn/src/components/modals/ButtonsModal.js
+++ packages/edge-login-ui-rn/src/components/modals/ButtonsModal.js
@@ -3,33 +3,68 @@
 import * as React from 'react'
 import { type AirshipBridge } from 'react-native-airship'
 
+import { showError } from '../services/AirshipInstance.js'
+import { ModalCloseArrow } from '../themed/ModalParts.js'
+import {
+  type PromiseButtonType,
+  PromiseButton
+} from '../themed/PromiseButton.js'
 import { PrimaryButton, SecondaryButton } from '../themed/ThemedButtons.js'
 import { ThemedModal } from '../themed/ThemedModal.js'
 import { MessageText, TitleText } from '../themed/ThemedText.js'
 
 type ButtonInfo = {
   label: string,
-  type?: 'primary' | 'secondary'
+  type?: PromiseButtonType,
+
+  // The modal will show a spinner as long as this promise is pending.
+  // Returning true will dismiss the modal,
+  // but returning false will leave the modal up.
+  // Although multiple buttons can be spinning at once,
+  // a spinning button cannot be clicked again until the promise resolves.
+  onPress?: () => Promise<boolean>
 }
 
 export function ButtonsModal<Buttons: { [key: string]: ButtonInfo }>(props: {
+  children?: React.Node,
   bridge: AirshipBridge<$Keys<Buttons> | void>,
-  title?: string,
+  buttons: Buttons,
+  closeArrow?: boolean,
   message?: string,
-  buttons: Buttons
+  title?: string
 }) {
-  const { bridge, title, message, buttons } = props
+  const {
+    bridge,
+    buttons,
+    children,
+    closeArrow = false,
+    message,
+    title
+  } = props
+  const handleCancel = () => bridge.resolve(undefined)
 
   return (
-    <ThemedModal
-      bridge={bridge}
-      onCancel={() => bridge.resolve(undefined)}
-      paddingRem={1}
-    >
+    <ThemedModal bridge={bridge} onCancel={handleCancel} paddingRem={1}>
       {title != null ? <TitleText>{title}</TitleText> : null}
       {message != null ? <MessageText>{message}</MessageText> : null}
+      {children}
       {Object.keys(buttons).map(key => {
-        const { label, type = 'primary' } = buttons[key]
+        const { type = 'primary', label, onPress } = buttons[key]
+
+        if (onPress != null) {
+          return (
+            <PromiseButton
+              key={key}
+              type={buttons[key].type}
+              label={buttons[key].label}
+              onPress={onPress}
+              onReject={showError}
+              onResolve={result => {
+                if (result) bridge.resolve(key)
+              }}
+            />
+          )
+        }
 
         switch (type) {
           case 'primary':
@@ -54,6 +89,7 @@ export function ButtonsModal<Buttons: { [key: string]: ButtonInfo }>(props: {
             return null
         }
       })}
+      {closeArrow ? <ModalCloseArrow onPress={handleCancel} /> : null}
     </ThemedModal>
   )
 }
---
+++ packages/edge-login-ui-rn/src/components/themed/PromiseButton.js
@@ -0,0 +1,74 @@
+// @flow
+
+import * as React from 'react'
+
+import { PrimaryButton, SecondaryButton } from './ThemedButtons'
+
+export type PromiseButtonType = 'primary' | 'secondary'
+
+type Props<T> = {|
+  type?: PromiseButtonType,
+  label: string,
+
+  // The button will show a spinner as long as this is pending.
+  // It will not be possible to press the button again while this is the case.
+  onPress: () => Promise<T>,
+  onResolve: (result: T) => void,
+  onReject: (error: mixed) => void
+|}
+type State = {|
+  spinning: boolean
+|}
+
+export class PromiseButton<T> extends React.Component<Props<T>, State> {
+  constructor(props: Props<T>) {
+    super(props)
+    this.state = { spinning: false }
+  }
+
+  handlePress = () => {
+    const { onPress, onResolve, onReject } = this.props
+
+    this.setState({ spinning: true })
+    onPress().then(
+      result => {
+        this.setState({ spinning: false })
+        onResolve(result)
+      },
+      error => {
+        this.setState({ spinning: false })
+        onReject(error)
+      }
+    )
+  }
+
+  render() {
+    const { label, type = 'primary' } = this.props
+    const { spinning } = this.state
+
+    switch (type) {
+      case 'primary':
+        return spinning ? (
+          <PrimaryButton marginRem={0.5} spinner />
+        ) : (
+          <PrimaryButton
+            label={label}
+            onPress={this.handlePress}
+            marginRem={0.5}
+          />
+        )
+      case 'secondary':
+        return spinning ? (
+          <SecondaryButton marginRem={0.5} spinner />
+        ) : (
+          <SecondaryButton
+            label={label}
+            onPress={this.handlePress}
+            marginRem={0.5}
+          />
+        )
+      default:
+        return null
+    }
+  }
+}
