8e177dd8bbef95df3983244c905c72d709a90b3a
William Swanson <swansontec@gmail.com>
2021-04-27 11:22:57 -0700

    rn: Move password-only styles into the scene

--- packages/edge-login-ui-rn/src/components/screens/PasswordLoginScreen.js
+++ packages/edge-login-ui-rn/src/components/screens/PasswordLoginScreen.js
@@ -248,7 +248,7 @@ class PasswordLoginScreenComponent extends React.Component<Props, State> {
     return (
       <UserListItem
         data={data.item}
-        style={styles.inputWithDrop.listItem}
+        style={styles.listItem}
         onClick={this.handleSelectUser}
         onDelete={this.handleDelete}
       />
@@ -400,8 +400,64 @@ const styles = {
     width: '100%',
     alignItems: 'center'
   },
-  input2: Styles.MaterialInput,
-  inputWithDrop: Styles.MaterialInputWithDrop,
+  input2: {
+    container: {
+      position: 'relative',
+      width: '70%',
+      minHeight: scale(60)
+    },
+    baseColor: Constants.WHITE,
+    tintColor: Constants.ACCENT_MINT,
+    errorColor: Constants.ACCENT_RED,
+    textColor: Constants.WHITE,
+    affixTextStyle: {
+      color: Constants.WHITE
+    },
+    titleTextStyle: {
+      color: Constants.WHITE
+    }
+  },
+  listItem: {
+    container: {
+      height: scale(40),
+      width: '100%',
+      backgroundColor: Constants.WHITE,
+      borderBottomColor: Constants.GRAY_4,
+      borderBottomWidth: 1,
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    textComtainer: {
+      flex: 9,
+      height: '100%',
+      flexDirection: 'column',
+      justifyContent: 'space-around'
+    },
+    iconButton: {
+      container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        height: '100%'
+      },
+      icon: {
+        color: Constants.PRIMARY
+      },
+      iconPressed: {
+        color: Constants.SECONDARY
+      },
+      iconSize: scale(Constants.FONTS.defaultFontSize),
+      underlayColor: Constants.TRANSPARENT
+    },
+    text: {
+      color: Constants.PRIMARY,
+      backgroundColor: Constants.TRANSPARENT,
+      fontFamily: Constants.FONTS.fontFamilyRegular,
+      marginLeft: scale(8),
+      fontSize: scale(Constants.FONTS.defaultFontSize)
+    }
+  },
   forgotButton: {
     upStyle: Styles.TextOnlyButtonUpStyle,
     upTextStyle: {
