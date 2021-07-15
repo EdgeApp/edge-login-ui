ee6c8c3230d3a535758a055544d0b3482e7dfd94
William Swanson <swansontec@gmail.com>
2021-07-14 18:43:13 -0700

    rn: Simplify the `Fade` component

--- packages/edge-login-ui-rn/src/components/themed/Fade.js
+++ packages/edge-login-ui-rn/src/components/themed/Fade.js
@@ -2,66 +2,53 @@
 
 import * as React from 'react'
 import Animated, {
-  interpolate,
   runOnJS,
   useAnimatedStyle,
   useSharedValue,
   withTiming
 } from 'react-native-reanimated'
 
-import { useEffect, useRef, useState } from '../../util/hooks'
+import { useEffect, useState } from '../../util/hooks.js'
 
 type Props = {
+  children: React.Node,
+
+  // True to make the contents visible:
   visible: boolean,
-  hidden?: boolean,
-  children: React.Node
+
+  // Animation duration, in ms:
+  duration?: number,
+
+  // True to simply make the contents transparent, rather than unmount them:
+  hidden?: boolean
 }
 
-const FadeComponent = ({ visible: propsVisible, hidden, children }: Props) => {
-  const firstRender = useRef(true)
+const FadeComponent = (props: Props) => {
+  const { children, duration = 500, visible, hidden } = props
+
+  // We always fade in on first mount:
   const opacity = useSharedValue(0)
-  const [visible, setVisible] = useState<boolean>(propsVisible)
-  const [prevVisible, setPrevVisible] = useState<boolean>(propsVisible)
-
-  const animate = (toValue: number) => {
-    if (toValue === 0.5) setVisible(true)
-
-    opacity.value = withTiming(
-      toValue,
-      {
-        duration: 500
-      },
-      (isComplete: boolean) => {
-        if (!isComplete) return
-
-        runOnJS(setVisible)(toValue === 0.5)
-      }
-    )
-  }
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => animate(propsVisible ? 0.5 : 0), [])
-
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false
-      return
-    }
-
-    if (propsVisible !== prevVisible) {
-      animate(propsVisible ? 0.5 : 1)
-      setPrevVisible(propsVisible)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [propsVisible])
-
   const style = useAnimatedStyle(() => ({
-    opacity: interpolate(opacity.value, [0, 0.5, 1], [0, 1, 0])
+    opacity: opacity.value
   }))
+
+  // Animate the opacity & child lifecycle based on the visibility toggle:
+  const [mounted, setMounted] = useState<boolean>(visible)
+  useEffect(() => {
+    if (visible) {
+      setMounted(true)
+      opacity.value = withTiming(1, { duration })
+    } else {
+      opacity.value = withTiming(0, { duration }, (isComplete: boolean) => {
+        if (!isComplete) return
+        runOnJS(setMounted)(false)
+      })
+    }
+  }, [duration, opacity, visible])
 
   return (
     <Animated.View style={style}>
-      {hidden || visible ? children : null}
+      {mounted || hidden ? children : null}
     </Animated.View>
   )
 }
