a2f80a753f8568c9f679ac6f70e8242d49ce64e2
William Swanson <swansontec@gmail.com>
2021-07-27 12:55:07 -0700

    rn: Fix build issues caused by typescript upgrade

--- packages/edge-login-ui-rn/src/components/common/ImageButton.tsx
+++ packages/edge-login-ui-rn/src/components/common/ImageButton.tsx
@@ -1,4 +1,3 @@
-// = @flow
 import * as React from 'react'
 import { Image, TouchableWithoutFeedback, View } from 'react-native'
 
--- packages/edge-login-ui-rn/src/hooks/useClearOnUnmount.ts
+++ packages/edge-login-ui-rn/src/hooks/useClearOnUnmount.ts
@@ -1,9 +1,10 @@
+import * as React from 'react'
+
 import { Airship } from '../components/services/AirshipInstance'
-import { useEffect } from '../util/hooks'
 
 /**
  * Clears modals when the component unmounts.
  */
 export function useClearOnUnmount() {
-  useEffect(() => Airship.clear, [])
+  React.useEffect(() => Airship.clear, [])
 }
