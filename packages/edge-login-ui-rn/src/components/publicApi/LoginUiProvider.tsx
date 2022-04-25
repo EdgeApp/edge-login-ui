9504f5d6f8621f627082364d80627bb225795d67
Paul Puey <paul@edge.app>
2022-04-25 15:38:45 -0700

    Add optionalTheme prop to LoginScreen
    Add asOptionalTheme cleaner. This is a duplicate of the Theme but with all parameters optional for the purpose of cleaning the optionalTheme prop.
    fixup! theme
    fixup! Use Theme in cleaner
    
    Also remove console.log
    fixup! Move theme prop to LoginUiProvider

--- packages/edge-login-ui-rn/src/components/publicApi/LoginUiProvider.tsx
+++ packages/edge-login-ui-rn/src/components/publicApi/LoginUiProvider.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 
+import { asOptionalTheme, Theme } from '../../types/Theme'
 import { Airship } from '../services/AirshipInstance'
-import { ThemeProvider } from '../services/ThemeContext'
+import { changeTheme, getTheme, ThemeProvider } from '../services/ThemeContext'
 
 /**
  * We use this context to determine if `LoginUiProvider` is mounted.
@@ -9,6 +10,7 @@ import { ThemeProvider } from '../services/ThemeContext'
 const loginUiContext = React.createContext(false)
 
 interface Props {
+  overrideTheme?: Theme
   children: React.ReactNode
 }
 
@@ -18,6 +20,24 @@ interface Props {
  * theme customizations.
  */
 export function LoginUiProvider(props: Props): JSX.Element {
+  if (props.overrideTheme != null) {
+    // Error check the theme but don't use the return value as the cleaner sets missing parameters
+    // to undefined which stomps on top of the oldTheme
+    asOptionalTheme(props.overrideTheme)
+
+    const overrideTheme = props.overrideTheme
+    const oldTheme = getTheme()
+    const tsHackTheme: any = { ...oldTheme, ...overrideTheme }
+    const newTheme: Theme = tsHackTheme
+
+    if (JSON.stringify(oldTheme) !== JSON.stringify(newTheme)) {
+      changeTheme(newTheme)
+    }
+    console.log(`oldTheme`, JSON.stringify(oldTheme, null, 2))
+    console.log(`overrideTheme`, JSON.stringify(overrideTheme, null, 2))
+    console.log(`newTheme`, JSON.stringify(newTheme, null, 2))
+  }
+
   return (
     <loginUiContext.Provider value>
       <ThemeProvider>
--- packages/edge-login-ui-rn/src/types/Theme.ts
+++ packages/edge-login-ui-rn/src/types/Theme.ts
@@ -1,3 +1,152 @@
+import {
+  asArray,
+  asNumber,
+  asObject,
+  asOptional,
+  asString,
+  asValue,
+  Cleaner
+} from 'cleaners'
+
+const asFunction: Cleaner<() => any> = raw => {
+  if (typeof raw === 'function') return raw
+  throw new TypeError()
+}
+
+const asThemeShadowParams = asObject({
+  shadowColor: asString,
+  shadowOffset: asObject({
+    width: asNumber,
+    height: asNumber
+  }),
+  shadowOpacity: asNumber,
+  shadowRadius: asNumber,
+  elevation: asNumber
+})
+type ThemeShadowParams = ReturnType<typeof asThemeShadowParams>
+
+const asTextShadowParams = asObject({
+  textShadowColor: asString,
+  textShadowOffset: asObject({
+    width: asNumber,
+    height: asNumber
+  }),
+  textShadowRadius: asNumber
+})
+type TextShadowParams = ReturnType<typeof asTextShadowParams>
+
+const asGradientCoords = asObject({
+  x: asNumber,
+  y: asNumber
+})
+type GradientCoords = ReturnType<typeof asGradientCoords>
+
+export const themeNoShadow: ThemeShadowParams = {
+  shadowColor: '#000000',
+  shadowOffset: {
+    width: 0,
+    height: 0
+  },
+  shadowOpacity: 0,
+  shadowRadius: 0,
+  elevation: 0
+}
+
+export const textNoShadow: TextShadowParams = {
+  textShadowColor: '#000000',
+  textShadowOffset: {
+    width: 0,
+    height: 0
+  },
+  textShadowRadius: 0
+}
+
+export const asOptionalTheme = asObject<Partial<Theme>>({
+  // The app scaling factor, which is the height of "normal" text:
+  rem: asOptional(asFunction),
+
+  fontFamily: asOptional(asString),
+  fontWeightBold: asOptional(
+    asValue(
+      'normal',
+      'bold',
+      '100',
+      '200',
+      '300',
+      '400',
+      '500',
+      '600',
+      '700',
+      '800',
+      '900'
+    )
+  ),
+
+  // Icons
+  icon: asOptional(asString),
+  iconDeactivated: asOptional(asString),
+  iconTappable: asOptional(asString),
+
+  // Background colors:
+  backgroundGradientColors: asArray(asString),
+
+  // Modal:
+  modal: asOptional(asString),
+  modalBlurType: asOptional(asValue('light', 'dark')),
+
+  // Text colors:
+  primaryText: asOptional(asString),
+  secondaryText: asOptional(asString),
+  dangerText: asOptional(asString),
+  warningText: asOptional(asString),
+  linkText: asOptional(asString),
+  positiveText: asOptional(asString),
+
+  // Tile:
+  tileBackground: asOptional(asString),
+
+  // Button colors:
+  primaryButtonOutline: asOptional(asString),
+  // primaryButton: asOptional(asString),
+  primaryButtonText: asOptional(asString),
+
+  secondaryButtonOutline: asOptional(asString),
+  // secondaryButton: asOptional(asString),
+  secondaryButtonText: asOptional(asString),
+
+  alertModalPrimaryButtonOutline: asOptional(asString),
+  alertModalPrimaryButton: asOptional(asString),
+  alertModalPrimaryButtonText: asOptional(asString),
+
+  alertModalTertiaryButtonOutline: asOptional(asString),
+  alertModalTertiaryButton: asOptional(asString),
+  alertModalTertiaryButtonText: asOptional(asString),
+
+  // Dropdown colors:
+  dropdownWarning: asOptional(asString),
+  dropdownError: asOptional(asString),
+  dropdownText: asOptional(asString),
+
+  // Security alert modal:
+  securityAlertModalHeaderCircle: asOptional(asString),
+  securityAlertModalDangerIcon: asOptional(asString),
+  securityAlertModalWarningIcon: asOptional(asString),
+  securityAlertModalRowBorder: asOptional(asString),
+  securityAlertModalText: asOptional(asString),
+
+  // Lines
+  lineDivider: asOptional(asString),
+  thinLineWidth: asOptional(asNumber),
+  mediumLineWidth: asOptional(asNumber),
+
+  // Font
+  fontFaceDefault: asOptional(asString),
+  fontFaceBold: asOptional(asString),
+  fontFaceSymbols: asOptional(asString)
+})
+
+export type OptionalTheme = ReturnType<typeof asOptionalTheme>
+
 type FontWeight =
   | 'normal'
   | 'bold'
